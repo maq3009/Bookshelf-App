3843e22f186f54c05726da798a683d1b4449a86a
maq3009 <106465903+maq3009@users.noreply.github.com>
2023-04-16 05:46:09 -0400

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,17 @@
+
+//STILL WORKING ON THSI SCRIPT////////
+
+
+
+// const favoriteBooks = [];
+
+// class Favorites {
+//     static favoriteBook(el) {
+//         const list = document.querySelector("#book-list");
+//         if(el.classList.contains("favorite")) {
+//             favoriteBooks.push(el.parentElement.parentElement);
+//             list.appendChild(favoriteBooks);
+//         }
+        
+// }
+// }
